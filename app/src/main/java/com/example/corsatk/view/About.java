4e7e502a25cd16f5bdc82f1be861e09f58a3c641
bola <bolatalaat055@gmail.com>
2017-06-16 04:47:14 +0200

    Ready get help

---
+++ app/src/main/java/com/example/corsatk/view/About.java
@@ -0,0 +1,95 @@
+package com.example.corsatk.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatDelegate;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.example.corsatk.controller.Item;
+import com.example.corsatk.R;
+import com.example.corsatk.adapters.ItemAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class About extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private ItemAdapter itemAdapter;
+    private GridLayoutManager gridLayoutManager;
+    private List<Item> items;
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.app_bar);
+        TextView mTitle = (TextView) toolbar.findViewById(R.id.toolbar_title);
+        mTitle.setText("About");
+
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
+
+        initItemsData();
+
+        gridLayoutManager = new GridLayoutManager(this, 1);
+        itemAdapter = new ItemAdapter(items, gridLayoutManager);
+        recyclerView = (RecyclerView) findViewById(R.id.rv);
+        recyclerView.setAdapter(itemAdapter);
+        recyclerView.setLayoutManager(gridLayoutManager);
+    }
+    private void initItemsData() {
+        items = new ArrayList<>(6);
+        items.add(new Item(R.drawable.fahad, "DR.Fahad ", "Instructor", "01281214755"));
+        items.add(new Item(R.drawable.farid, "DR.Farid", "Instructor", " 01222435861"));
+        items.add(new Item(R.drawable.bola, "Bola", "android developer" , "bolatalaat055@gmail.com "));
+        items.add(new Item(R.drawable.tony, "Antonious", "android developer", "AntoniousJacoub@gmail.coom"));
+        items.add(new Item(R.drawable.ahmed, "Ahmed", "graphic designer", "ahmedshanzlezeh@outlook.com"));
+        items.add(new Item(R.drawable.mostafa, "Mostafa", "web developer", "mostafahasieb13@yahoo.com"));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.about_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.menu_switch_layout) {
+            switchLayout();
+            switchIcon(item);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void switchLayout() {
+        switch (gridLayoutManager.getSpanCount()) {
+            case 1:
+                gridLayoutManager.setSpanCount(3);
+                break;
+            case 3:
+                gridLayoutManager.setSpanCount(1);
+                break;
+            default:
+                gridLayoutManager.setSpanCount(1);
+                break;
+        }
+        itemAdapter.notifyItemRangeChanged(0, itemAdapter.getItemCount());
+    }
+
+    private void switchIcon(MenuItem item) {
+        if (gridLayoutManager.getSpanCount() == 3) {
+            item.setIcon(getResources().getDrawable(R.drawable.c_plus));
+        } else {
+            item.setIcon(getResources().getDrawable(R.drawable.c_plus));
+        }
+    }
+}
